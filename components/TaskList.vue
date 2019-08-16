891871f7d99c7cc53cce1564acb24ed62a7d8d4b
nof <sakape@gmail.com>
2019-08-16 16:33:52 +0900

    feat: タスク

---
+++ components/TaskList.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="list-group list-group-flush">
+    <li 
+      v-for="task in tasks" 
+      :key="task.id"
+      class="list-group-item ml-3"
+    >
+      <input 
+        :id="`task-checkbox-${task.id}`"
+        v-model="task.finished" 
+        class="form-check-input"
+        type="checkbox" >
+      <label :for="`task-checkbox-${task.id}`">
+        {{ task.title }}
+      </label>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    tasks: {
+      type: Array,
+      required: true,
+      default: () => { return [] }
+    }
+  }
+}
+</script>
---
+++ pages/tasks/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <section class="container pt-5">
+
+    <div class="card mb-5">
+      <div class="card-header">
+        やること
+      </div>
+      <task-list :tasks="unfinishedTasks" />
+    </div>
+
+    <div class="card">
+      <div class="card-header">
+        やったこと
+      </div>
+      <task-list :tasks="finishedTasks" />
+    </div>
+
+  </section>
+</template>
+
+<script>
+import TaskList from '~/components/TaskList.vue'
+
+export default {
+  components: { TaskList },
+  data() {
+    return {
+      tasks: [
+        { id: 1, title: '牛乳を買う', finished: true },
+        { id: 2, title: 'たまごを買う', finished: false },
+      ]
+    }
+  },
+  computed: {
+    finishedTasks() {
+      return this.tasks.filter((task) => { return task.finished })
+    },
+    unfinishedTasks() {
+      return this.tasks.filter((task) => { return !task.finished })
+    }
+  },
+}
+</script>
